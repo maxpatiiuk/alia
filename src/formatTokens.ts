788325c7ca195a6ef5390c19a006da66b44cead7
Max Patiiuk <max@patii.uk>
2022-08-25 22:20:58 -0500

    Implement output formatter

---
+++ src/formatTokens.ts
@@ -0,0 +1,24 @@
+import { formatPosition } from './formatErrors.js';
+import type { Tokens } from './tokenize/tokens.js';
+import { tokenLabels } from './tokenize/tokens.js';
+import type { Position, Token } from './tokenize/types.js';
+import type { RA } from './utils/types.js';
+
+export const formatTokens = (
+  tokens: RA<Token>,
+  positionResolver: (simplePosition: number) => Position
+): string =>
+  tokens
+    .map(
+      ({ type, data, simplePosition }) =>
+        `${formatName(type, data)}: [${formatPosition(
+          positionResolver(simplePosition)
+        )}]`
+    )
+    .join('\n');
+
+function formatName<T extends keyof Tokens>(type: T, data: Tokens[T]): string {
+  const label = type in tokenLabels ? tokenLabels[type] ?? type : type;
+  const payload = 'literal' in data ? `:${data.literal}` : '';
+  return `${label}${payload}`;
+}
