57b6642324a5a0f445f795cbe893894cd5d6ecc4
Max Patiiuk <max@patii.uk>
2022-08-25 22:21:12 -0500

    Implement error formatter

---
+++ src/formatErrors.ts
@@ -0,0 +1,21 @@
+import type { ParseError, Position } from './tokenize/types.js';
+import type { RA } from './utils/types.js';
+
+export const formatErrors = (
+  errors: RA<ParseError>,
+  positionResolver: (simplePosition: number) => Position
+): string =>
+  errors
+    .map(
+      ({ start, end, message }) =>
+        `FATAL [${formatPosition(positionResolver(start))}-${formatPosition(
+          positionResolver(end)
+        )}]: ${message}`
+    )
+
+    .join('\n');
+
+export const formatPosition = ({
+  lineNumber,
+  columnNumber,
+}: Position): string => `${lineNumber},${columnNumber}`;
