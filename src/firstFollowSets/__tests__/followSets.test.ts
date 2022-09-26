4f06f0bae60a268750be95913c12fad4a32703c9
Max Patiiuk <max@patii.uk>
2022-09-25 20:04:07 -0500

    Implement follow sets utility

---
+++ src/firstFollowSets/__tests__/followSets.test.ts
@@ -0,0 +1,66 @@
+import { theories } from '../../../../t2/src/tests/utils.js';
+import {
+  exportsForTests,
+  findAllIndexesOf,
+  getFollowSets,
+} from '../followSets.js';
+
+const { findTerminalEndings } = exportsForTests;
+
+theories(getFollowSets, [
+  {
+    in: [
+      {
+        s: [['a'], ['b', 'r']],
+        q: [],
+        r: [
+          ['q', 'c'],
+          ['q', 's'],
+          ['q', 'q'],
+        ],
+      },
+      {
+        '["s"]': new Set<string>(['a', 'b']),
+        '["q"]': new Set<string>(['']),
+        '["r"]': new Set<string>(['c', 'a', 'b', '']),
+        '["a"]': new Set(['a']),
+        '["b"]': new Set(['b']),
+        '["c"]': new Set(['c']),
+        '["b","r"]': new Set(['b']),
+        '["q","c"]': new Set<string>(['c']),
+        '["q","s"]': new Set<string>(['a', 'b']),
+        '["q","q"]': new Set<string>(['']),
+        '[]': new Set(['']),
+      },
+    ],
+    out: {
+      s: new Set(['']),
+      q: new Set(['c', 'a', 'b', '']),
+      r: new Set(['']),
+    },
+  },
+]);
+
+theories(findTerminalEndings, [
+  {
+    in: [
+      {
+        a: [['b', 'a', 'c', 'a', 'd']],
+        b: [['a'], []],
+      },
+      'a',
+    ],
+    out: [
+      { terminalName: 'a', ending: ['c', 'a', 'd'] },
+      { terminalName: 'a', ending: ['d'] },
+      { terminalName: 'b', ending: [] },
+    ],
+  },
+]);
+
+theories(findAllIndexesOf, [
+  { in: [[], 'a'], out: [] },
+  { in: [['a'], 'a'], out: [0] },
+  { in: [['b'], 'a'], out: [] },
+  { in: [[1, 0, 2, 0, 3, 0], 0], out: [1, 3, 5] },
+]);
---
+++ src/firstFollowSets/followSets.ts
@@ -0,0 +1,71 @@
+import type { IR, RA } from '../../../t2/src/utils/types.js';
+import { filterArray } from '../../../t2/src/utils/types.js';
+import type { PureGrammar } from './firstSets.js';
+import { lineToString, saturate } from './firstSets.js';
+
+/**
+ * Compute follow sets for all non-terminals
+ */
+export const getFollowSets = (
+  grammar: PureGrammar,
+  firstSets: IR<ReadonlySet<string>>
+): IR<ReadonlySet<string>> =>
+  saturate(
+    saturateFollowSets.bind(undefined, grammar, firstSets),
+    Object.fromEntries(Object.keys(grammar).map((key) => [key, new Set()]))
+  );
+
+/**
+ * An instance of the saturating algorithm for finding follow sets
+ */
+const saturateFollowSets = (
+  grammar: PureGrammar,
+  firstSets: IR<ReadonlySet<string>>,
+  followSets: IR<ReadonlySet<string>>
+): IR<ReadonlySet<string>> =>
+  Object.fromEntries(
+    Object.keys(followSets).map((key) => {
+      const followSet = new Set<string>();
+
+      // Is start non-terminal
+      if (Object.keys(grammar)[0] === key) followSet.add('');
+
+      findTerminalEndings(grammar, key).forEach(({ terminalName, ending }) => {
+        const rawFirstSet = Array.from(firstSets[lineToString(ending)] ?? []);
+        const firstSet = rawFirstSet.filter((part) => part !== '');
+        [
+          ...(firstSet.length === 0 || rawFirstSet.includes('')
+            ? Array.from(followSets[terminalName])
+            : []),
+          ...firstSet,
+        ].map((item) => followSet.add(item));
+      });
+
+      return [key, followSet];
+    })
+  );
+
+/**
+ * Return all parts of all lines to the right of a given terminal
+ */
+const findTerminalEndings = (
+  grammar: PureGrammar,
+  key: string
+): RA<{ readonly terminalName: string; readonly ending: RA<string> }> =>
+  Object.entries(grammar).flatMap(([terminalName, lines]) =>
+    lines.flatMap((line) =>
+      findAllIndexesOf(line, key)
+        .map((index) => line.slice(index + 1))
+        .map((ending) => ({ terminalName, ending }))
+    )
+  );
+
+export const findAllIndexesOf = <T>(array: RA<T>, search: T): RA<number> =>
+  filterArray(
+    array.map((item, index) => (item === search ? index : undefined))
+  );
+
+export const exportsForTests = {
+  findTerminalEndings,
+  saturateFollowSets,
+};
