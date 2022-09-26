9a5d27edba233bce7980b59a182565363b4af56d
Max Patiiuk <max@patii.uk>
2022-09-25 20:03:53 -0500

    Implement first sets utility

---
+++ src/firstFollowSets/__tests__/firstSets.test.ts
@@ -0,0 +1,156 @@
+import { wrapLine } from '../../../../t2/src/processGrammar/leftFactor.js';
+import { theories } from '../../../../t2/src/tests/utils.js';
+import {
+  exportsForTests,
+  findAllSubsets,
+  getFirstSets,
+  toPureGrammar,
+  lineToString,
+} from '../firstSets.js';
+
+const { saturate, getSetsLength, buildSet } = exportsForTests;
+
+describe('a', () => {
+  theories(getFirstSets, [
+    {
+      in: [
+        {
+          s: [['a'], ['b', 'r']],
+          q: [[]],
+          r: [
+            ['q', 'c'],
+            ['q', 's'],
+            ['q', 'q'],
+          ],
+        },
+      ],
+      out: {
+        '["s"]': new Set<string>(['a', 'b']),
+        '["q"]': new Set<string>(['']),
+        '["r"]': new Set<string>(['c', 'a', 'b', '']),
+        '["a"]': new Set(['a']),
+        '["b"]': new Set(['b']),
+        '["c"]': new Set(['c']),
+        '["b","r"]': new Set(['b']),
+        '["q","c"]': new Set<string>(['c']),
+        '["q","s"]': new Set<string>(['a', 'b']),
+        '["q","q"]': new Set<string>(['']),
+        '[]': new Set(['']),
+      },
+    },
+    {
+      name: 'non-terminal with epsilon results in next first set being appended',
+      in: [{ a: [['b', 'c']], b: [['d'], []] }],
+      out: {
+        '["a"]': new Set(['d', 'c']),
+        '["b","c"]': new Set(['d', 'c']),
+        '["b"]': new Set(['d', '']),
+        '["c"]': new Set(['c']),
+        '["d"]': new Set(['d']),
+        '[]': new Set(['']),
+      },
+    },
+    {
+      name: 'if all parts have epsilons, result has epsilon too',
+      in: [
+        {
+          a: [['b', 'c']],
+          b: [['d'], []],
+          c: [['e'], []],
+        },
+      ],
+      out: {
+        '["a"]': new Set(['d', 'e', '']),
+        '["b","c"]': new Set(['d', 'e', '']),
+        '["c"]': new Set(['e', '']),
+        '["b"]': new Set(['d', '']),
+        '["d"]': new Set(['d']),
+        '["e"]': new Set(['e']),
+        '[]': new Set(['']),
+      },
+    },
+  ]);
+});
+
+theories(findAllSubsets, [
+  {
+    in: [[]],
+    out: [],
+  },
+  {
+    in: [[1, 2, 3]],
+    out: [[1], [1, 2], [1, 2, 3], [2], [2, 3], [3]],
+  },
+]);
+
+theories(toPureGrammar, [
+  {
+    in: [
+      {
+        a: [[{ type: 'ActionReference', number: 2 }]],
+        b: wrapLine([['b']]),
+      },
+    ],
+    out: {
+      a: [[]],
+      b: [['b']],
+    },
+  },
+]);
+
+theories(lineToString, [
+  {
+    in: [[]],
+    out: '[]',
+  },
+  {
+    in: [['a', 'b']],
+    out: '["a","b"]',
+  },
+]);
+
+theories(saturate, [
+  {
+    in: [
+      (sets) =>
+        sets.a.size < 5
+          ? { a: new Set([...Array.from(sets.a), sets.a.size.toString()]) }
+          : { ...sets },
+      { a: new Set([]) },
+    ],
+    out: { a: new Set(['0', '1', '2', '3', '4']) },
+  },
+]);
+
+theories(getSetsLength, [
+  {
+    in: [{}],
+    out: 0,
+  },
+  {
+    in: [{ a: new Set(['a', 'b']) }],
+    out: 2,
+  },
+]);
+
+theories(buildSet, [
+  {
+    in: [{ a: [['b']] }, {}, 'b'],
+    out: new Set(['b']),
+  },
+  {
+    in: [
+      { a: [['b'], ['c', 'd']] },
+      {
+        '["b"]': new Set(['b']),
+        '["c","d"]': new Set(['d', 'e']),
+      },
+      'a',
+    ],
+    out: new Set(['b', 'd', 'e']),
+  },
+  {
+    in: [{ a: [[]] }, { '[]': new Set([]) }, 'a'],
+    out: new Set([]),
+  },
+]);
---
+++ src/firstFollowSets/firstSets.ts
@@ -0,0 +1,123 @@
+import type { Grammar } from '../../../t2/src/grammar/types.js';
+import type { IR, RA } from '../../../t2/src/utils/types.js';
+import { filterArray } from '../../../t2/src/utils/types.js';
+
+/**
+ * Computer first sets for all non-terminals and sequences of terminals
+ */
+export function getFirstSets(grammar: PureGrammar): IR<ReadonlySet<string>> {
+  const allKeys = Array.from(
+    new Set(
+      [
+        [],
+        ...Object.keys(grammar).map((item) => [item]),
+        ...Object.values(grammar).flat().flatMap(findAllSubsets),
+      ].map(lineToString)
+    )
+  );
+
+  return saturate(
+    saturateFirstSets.bind(undefined, grammar),
+    Object.fromEntries(allKeys.map((key) => [key, new Set()]))
+  );
+}
+
+export const findAllSubsets = <T>(array: RA<T>): RA<RA<T>> =>
+  array.flatMap((_, index) =>
+    array
+      .slice(index)
+      .map((_, length) => array.slice(index, index + length + 1))
+  );
+
+export type PureGrammar = IR<RA<RA<string>>>;
+/**
+ * Remove actions from the grammar. Leave only terminals and non-terminals
+ */
+export const toPureGrammar = (grammar: Grammar): PureGrammar =>
+  Object.fromEntries(
+    Object.entries(grammar).map(([name, lines]) => [
+      name,
+      lines.map((line) =>
+        filterArray(
+          line.map((part) => (part.type === 'Part' ? part.name : undefined))
+        )
+      ),
+    ])
+  );
+
+export const lineToString = (line: RA<string>): string =>
+  JSON.stringify(filterArray(line));
+
+/**
+ * Call a hash-set producing function until the result does not change
+ */
+export function saturate(
+  callback: (sets: IR<ReadonlySet<string>>) => IR<ReadonlySet<string>>,
+  initialSets: IR<ReadonlySet<string>>
+): IR<ReadonlySet<string>> {
+  const initialLength = getSetsLength(initialSets);
+  const newSets = callback(initialSets);
+  return getSetsLength(newSets) === initialLength
+    ? newSets
+    : saturate(callback, newSets);
+}
+
+/**
+ * Calculate the total length of all sets
+ * Used to determine if the first sets have stabilized
+ */
+const getSetsLength = (firstSets: IR<ReadonlySet<string>>): number =>
+  Object.values(firstSets).reduce((sum, { size }) => sum + size, 0);
+
+/**
+ * Recursive algorithm for computing first sets.
+ * Stops when no more changes are made to the first sets
+ */
+const saturateFirstSets = (
+  grammar: PureGrammar,
+  firstSets: IR<ReadonlySet<string>>
+): IR<ReadonlySet<string>> =>
+  Object.fromEntries(
+    Object.keys(firstSets).map((key) => {
+      const parts = JSON.parse(key) as RA<string>;
+      if (parts.length === 0) return [key, new Set([''])];
+
+      const firstSet = new Set<string>();
+      const hasEpsilon = parts.every((part) => {
+        const newFirstSet = buildSet(grammar, firstSets, part);
+        Array.from(newFirstSet, (part) =>
+          part === '' ? undefined : firstSet.add(part)
+        );
+        return newFirstSet.has('');
+      });
+
+      if (hasEpsilon) firstSet.add('');
+      return [key, firstSet];
+    })
+  );
+
+/**
+ * Build a first set for a single terminal or non-terminal
+ */
+function buildSet(
+  grammar: PureGrammar,
+  firstSets: IR<ReadonlySet<string>>,
+  part: string
+): ReadonlySet<string> {
+  const isTerminal = !(part in grammar);
+  return new Set(
+    isTerminal
+      ? [part]
+      : grammar[part]
+          .map(lineToString)
+          .flatMap((line) => Array.from(firstSets[line]))
+  );
+}
+
+export const exportsForTests = {
+  lineToString,
+  saturateFirstSets,
+  buildSet,
+  saturate,
+  getSetsLength,
+};
