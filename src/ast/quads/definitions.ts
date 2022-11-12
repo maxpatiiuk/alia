90e71c8166bbad7478231aa42e871ff21fab5622
Max Patiiuk <max@patii.uk>
2022-11-12 10:17:20 -0600

    Fix loop not adding trailing GoTo

--- src/ast/quads/definitions.ts
+++ src/ast/quads/definitions.ts
@@ -447,7 +447,12 @@ export class ForQuad extends Quad {
     this.quads = [
       ...this.declaration,
       new LabelQuad(this.startLabel, new NopQuad()),
-      new IfQuad(this.condition, this.statements, undefined, this.endLabel),
+      new IfQuad(
+        this.condition,
+        [...this.statements, new GoToQuad(this.startLabel)],
+        undefined,
+        this.endLabel
+      ),
     ];
   }
 
