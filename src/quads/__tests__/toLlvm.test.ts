d0be9a07febd226ed0bdc908281b652ed0254c52
Max Patiiuk <max@patii.uk>
2022-12-04 10:09:21 -0600

    Add example test for LLVM

---
+++ src/quads/__tests__/toLlvm.test.ts
@@ -0,0 +1,20 @@
+import { nameParse, run, typeCheckAst } from '../../frontEnd.js';
+import { toQuads } from '../index.js';
+import { testProgram } from './testProgram.js';
+import { GlobalQuad } from '../definitions/GlobalQuad.js';
+
+test('toLlvm', async () => {
+  const ast = await run({
+    rawText: testProgram,
+  });
+  expect(ast).toBeDefined();
+  expect(typeof nameParse(ast!, false)).toBe('string');
+  expect(typeCheckAst(ast!, testProgram)).toHaveLength(0);
+  const quads = toQuads(ast!);
+
+  const globalQuad = quads[0];
+  if (!(globalQuad instanceof GlobalQuad))
+    throw new Error('Top level quad must be an instance of GlobalQuad');
+
+  expect(globalQuad.convertToLlvm(false)).toMatchSnapshot();
+});
